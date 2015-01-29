87b4b6df395772fa4483d67d6d4088a5258b6ff2
AJ <ajburns93@gmail.com>
2015-01-28 18:15:10 -0700

    working command execution

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 
 #define PROMPT "RedPizzaBaron> "
+#define STRLEN 1024
+#define MAX_ARGS 32
 
 int shell(char* command);
 int parseCommand(char* com);
 
 int main(int argc, char** argv) {
-	int i = 1;
-	for(; i < argc; i++) {
-		shell(argv[i]);
+	char tmpstr[STRLEN];
+	printf(PROMPT);
+	while(fgets(tmpstr, STRLEN, stdin)){
+		parseCommand(tmpstr);
+		printf(PROMPT);
 	}
 
 	return 0;
@@ -20,6 +28,35 @@ int shell(char* command) {
 	return 0;
 }
 
+/**
+ * Parses the command into proper format
+ * and then runs the command
+ * @argument com, the command to parse and run
+ * @returns 0 if successful, -1 if error
+ **/
 int parseCommand(char* com){
+	char* args[MAX_ARGS];
+	int i;
 
+	// set null terminator properly
+	for(i = 0; i < sizeof(com); i++){
+		if(com[i] == '\n' || com[i] == '\0'){
+			com[i] = '\0';
+			break;
+		}
+	}
+
+	// get first token
+	args[0] = strtok(com, " ");
+	for(i = 1; i < MAX_ARGS;i++){
+		args[i] = strtok(NULL, " ");
+		if(args[i] == NULL)
+			break;
+		
+	}
+
+	// execute command
+	execvp(args[0], args);
+
+	return 0;
 }
